c4d160df8d2d55dbd92373ca2a11861c33a82fd6
Doko Zero <17993323+dokozero@users.noreply.github.com>
2025-03-16 16:48:39 +0100

    Moved todo items in github project from the repo

--- dist/helpers/convertRelativeChromaToAbsolute/convertRelativeChromaToAbsolute.js
+++ dist/helpers/convertRelativeChromaToAbsolute/convertRelativeChromaToAbsolute.js
@@ -4,7 +4,7 @@ export default function convertRelativeChromaToAbsolute(props) {
     try {
         // First, we need to get the current maximum chroma for the given lightness and hue. For that, we clamp the color with an absolute chroma of 1, which is way out of display-p3 or rec2020 gamuts.
         const currentMaxChroma = clampChroma({ mode: 'oklch', l: lightness / 100, c: 1, h: hue }, 'oklch', colorSpace).c;
-        // Then, as we know the absolute chroma value of a relative one of 100%, we can get the absolute chroma from the requested relative one.
+        // Then, as we know the absolute chroma value of a relative one of 100%, we can get the absolute chroma from the requested relative one with a cross multiplication operation.
         return Number(((relativeChroma * currentMaxChroma) / 100).toFixed(6));
     }
     catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import { OKLCH_RELATIVE_CHROMA_REGEX } from './data.js'
 import getConvertedOklchCode from './helpers/getConvertedOklchCode/getConvertedOklchCode.js'
 
-// TODO
-// Test also if gradient 'in oklch' don't create a bug.
-// Work with 0.123 for L?
-// Opacity?
-// Add automatic sRGB fallback with chroma reduction method?
-//  Or another dedicated script?
-
 export default () => {
   return {
     postcssPlugin: 'postcss-oklch-relative-chroma',
